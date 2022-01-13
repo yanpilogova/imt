c9e22d52ed3494d5fba94b5c62806c84d14d97de
yanpilogova <anpilogova1986@gmail.com>
2022-01-13 14:17:52 +0700

    imt formula was fixed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate(int mass, double height) {
-        double imt = mass / (height * 2);
+        double imt = mass / (height * height);
         return imt;
     }
   }
